953cefd5e97f3e84dffedd168c77ff6a5c84769a
rednerank <narender.kumar@talentica.com>
2017-12-13 14:50:35 +0530

    add simple rest endpoints

--- index.js
+++ index.js
@@ -2,11 +2,55 @@ const express = require('express');
 const http = require('http');
 const morgan = require('morgan');
 
+const bodyParser = require('body-parser');
+
 const hostname = 'localhost';
 const port = 3000;
 
 const app = express();
 app.use(morgan('dev'));
+app.use(bodyParser.json());
+
+app.all('/dishes', (req, res, next) => {
+   res.statusCode = 200;
+   res.setHeader('Content-type', 'text/plain');
+   next();
+});
+
+app.get('/dishes', (req, res, next) => {
+   res.end('Will send all the dishes to you');
+});
+
+app.post('/dishes', (req, res, next) => {
+   res.end('will add the dish : '+req.body.description);
+});
+
+app.put('/dishes', (req, res, next) => {
+    res.statusCode =403;
+    res.end('put on dishes not supported!');
+});
+
+app.delete('/dishes', (req, res, next) => {
+    res.end('deliting all the dishes');
+});
+
+app.get('/dishes/:dishId', (req, res, next) => {
+    res.end('Will send all the dish : ' +req.params.dishId+ 'to you!');
+});
+
+app.post('/dishes/:dishId', (req, res, next) => {
+    res.statusCode =403;
+    res.end('POST operation not supported on /dishes/' +req.param.dishId);
+});
+
+app.put('/dishes/:dishId', (req, res, next) => {
+    res.write('updating the dish: '+req.params.dishId);
+    res.end('Will update the dishe: ' +req.body.name+ ' to you!');
+});
+
+app.delete('/dishes/:dishId', (req, res, next) => {
+    res.end('deliting dish: '+req.params.dishId);
+});
 
 app.use(express.static(__dirname+ '/public'));
 
